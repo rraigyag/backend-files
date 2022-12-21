2eef937e46faee08b2e7adc68b2603cffea2cfc0
Raigya Gnanashruthi <93989348+rraigyag@users.noreply.github.com>
2022-12-21 07:57:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,20 @@ const express = require('express')
 const dbConnect=require('./dbConnect')
 const cors=require('cors')
 
-const app = express()
-app.use(express.json())
-const path = require('path')
 
 
 const userRoute=require('./routes/usersRoute');
 const transactionsRoute=require('./routes/transactionsRoute')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
 app.use('/api/users/',userRoute)
 app.use('/api/transactions/',transactionsRoute)
-app.use(cors())
 
 const port =process.env.PORT || 5000
 
-if(process.env.NODE_ENV === 'production')
-{
-     app.use('/' , express.static('client/build'))
 
-     app.get('*' , (req, res)=>{
-         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-     })
-}
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Node Js server started at  port ${port}!`))
