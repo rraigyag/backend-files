48c3dcfbedd861b054bd910d7128626274afb31e
Raigya Gnanashruthi <93989348+rraigyag@users.noreply.github.com>
2023-04-27 13:28:01 +0530

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://home-expenses:home expenses@cluster0.gcvj3ut.mongodb.net/domestic-expenditure-auditing',
+mongoose.connect('mongodb+srv://home-expenses:home-expenses@cluster0.gcvj3ut.mongodb.net/domestic-expenditure-auditing',
 {useNewUrlParser:true,useUnifiedTopology:true}
 );
 const connection=mongoose.connection
